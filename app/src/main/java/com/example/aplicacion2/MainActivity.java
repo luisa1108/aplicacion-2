d53a2ac24001a0afd1d207a3fd25cd14ce1515b6
luisa1108 <159147642+luisa1108@users.noreply.github.com>
2024-02-08 09:28:50 -0500

    clase 2
    Aplicación con botones y texto, con nuevos metodos

--- app/src/main/java/com/example/aplicacion2/MainActivity.java
+++ app/src/main/java/com/example/aplicacion2/MainActivity.java
@@ -6,12 +6,38 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean result = true;
-    String name = "Bob";
+    TextView NombreUsuario;
+    Button btn1,btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NombreUsuario = (TextView) findViewById(R.id.usuario);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+
+        btn1.setOnClickListener(new.View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view){
+                NombreUsuario.setText("Presioné boton uno");
+            }
+        });
+
+        btn2.setOnClickListener(new.View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view){
+                NombreUsuario.setText("Presioné boton dos");
+            }
+        });
+
+    }
+
+    public double calcularArea(int l1,int l2){
+        return l1*l2;
     }
 }
